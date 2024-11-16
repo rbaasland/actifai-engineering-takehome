356293cfc2cba91f307bcd01a182e50a0f807b07
Ron Baasland <ron.baasland@gmail.com>
2024-11-16 13:52:35 -0500

    Create method to get sale metrics by day over a time period

--- libs/saleMetrics.js
+++ libs/saleMetrics.js
@@ -11,3 +11,35 @@ const pgclient = new Client({
 
 pgclient.connect();
 
+/**
+ * Get total sales by day over a time period
+ * @param {string} startDate The first number to add.
+ * @param {string} endDate The second number to add.
+ * @return {object} Total sales by day
+ */    
+const getSalesByDate = async function(startDate, endDate ) {
+    if (endDate < startDate) {
+        throw new Error('Invalid time period');
+    }
+    try {
+        const queryText = `
+            SELECT 
+                to_char(date, 'yyyy-mm-dd') as date,
+                sum(amount) as total
+            FROM sales 
+            WHERE
+                date >= $1 and date <= $2
+            GROUP BY date
+            ORDER BY date ASC`;
+        const params = [startDate, endDate];
+        const results = await pgclient.query(queryText, params);
+        return results.rows;
+    } catch(error) {
+        throw new Error('Query error');
+    }
+}
+
+module.exports = {
+    getSalesByDate,
+    getUserSalesByMonth
+}
