0b2ad880f0ad1026195c5666ccafc2d44579eb46
Ron Baasland <ron.baasland@gmail.com>
2024-11-16 14:15:17 -0500

    - Create database wrapper - Update queries in sales to use new wrapper

--- libs/saleMetrics.js
+++ libs/saleMetrics.js
@@ -1,15 +1,6 @@
 'use strict';
 
-const { Client } = require('pg');
-const pgclient = new Client({
-  host: 'db',
-  port: '5432',
-  user: 'user',
-  password: 'pass',
-  database: 'actifai'
-});
-
-pgclient.connect();
+const database = require('./database');
 
 /**
  * Get total sales by day over a time period
@@ -21,22 +12,19 @@ const getSalesByDate = async function(startDate, endDate ) {
     if (endDate < startDate) {
         throw new Error('Invalid time period');
     }
-    try {
-        const queryText = `
-            SELECT 
-                to_char(date, 'yyyy-mm-dd') as date,
-                sum(amount) as total
-            FROM sales 
-            WHERE
-                date >= $1 and date <= $2
-            GROUP BY date
-            ORDER BY date ASC`;
-        const params = [startDate, endDate];
-        const results = await pgclient.query(queryText, params);
-        return results.rows;
-    } catch(error) {
-        throw new Error('Query error');
-    }
+    
+    const queryText = `
+        SELECT 
+            to_char(date, 'yyyy-mm-dd') as date,
+            sum(amount) as total
+        FROM sales 
+        WHERE
+            date >= $1 and date <= $2
+        GROUP BY date
+        ORDER BY date ASC`;
+    const params = [startDate, endDate];
+    const results = await database.executeQuery(queryText, params);
+    return results.rows;
 }
 
 /**
@@ -49,31 +37,28 @@ const getUserSalesByMonth = async function(startDate, endDate ) {
     if (endDate < startDate) {
         throw new Error('Invalid time period');
     }
-    try {
-        const queryText = `
-            SELECT
-				s.user_id,
-				u.name,
-                to_char(date_trunc('month', s.date), 'yyyy-mm-dd') as sales_month,
-                sum(s.amount) as total_sales,
-                ROUND(AVG(s.amount), 2) as average_sale
-            FROM sales s
-            LEFT JOIN users u ON s.user_id = u.id
-            WHERE
-                s.date >= $1 and s.date <= $2
-            GROUP BY
-                sales_month, 
-                s.user_id,
-                u.name
-            ORDER BY
-                sales_month ASC,
-                s.user_id ASC`;
-        const params = [startDate, endDate];
-        const results = await pgclient.query(queryText, params);
-        return results.rows;
-    } catch(error) {
-        throw new Error('Query error');
-    }
+
+    const queryText = `
+        SELECT
+            s.user_id,
+            u.name,
+            to_char(date_trunc('month', s.date), 'yyyy-mm-dd') as sales_month,
+            sum(s.amount) as total_sales,
+            ROUND(AVG(s.amount), 2) as average_sale
+        FROM sales s
+        LEFT JOIN users u ON s.user_id = u.id
+        WHERE
+            s.date >= $1 and s.date <= $2
+        GROUP BY
+            sales_month, 
+            s.user_id,
+            u.name
+        ORDER BY
+            sales_month ASC,
+            s.user_id ASC`;
+    const params = [startDate, endDate];
+    const results = await database.executeQuery(queryText, params);
+    return results.rows;
 }
 
 module.exports = {
