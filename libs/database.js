cc957aaf521708335b222a3777445feb11fbee94
Ron Baasland <ron.baasland@gmail.com>
2024-11-16 22:58:59 -0500

    - Refactor datebase lib

--- libs/database.js
+++ libs/database.js
@@ -1,35 +1,36 @@
 'use strict';
+
 const { Client } = require('pg');
 
 const executeQuery = async (query, params ) => {
-    return await pgQuery(query, params);
+  return await pgQuery(query, params);
 };
 
 async function pgQuery(query, params) {
-    const pgclient = new Client({
-        host: 'db',
-        port: '5432',
-        user: 'user',
-        password: 'pass',
-        database: 'actifai'
-    });
+  const pgclient = new Client({
+    host: 'db',
+    port: '5432',
+    user: 'user',
+    password: 'pass',
+    database: 'actifai'
+  });
 
-    try {
-        pgclient.connect();
-    } catch(error) {
-        throw new Error('Database connection error' + JSON.stringify(error));
-    }
+  try {
+    pgclient.connect();
+  } catch(error) {
+    throw new Error('Database connection error' + JSON.stringify(error));
+  }
 
-    try {
-        const response = await pgclient.query({
-            text: query,
-            values: params
-        })
-        await pgclient.end();
-        return response;
-    } catch(error) {
-        throw new Error('Database query error: ' + JSON.stringify(error));
-    }
+  try {
+    const response = await pgclient.query({
+      text: query,
+      values: params
+    })
+    await pgclient.end();
+    return response;
+  } catch(error) {
+    throw new Error('Database query error: ' + JSON.stringify(error));
+  }
 };
 
 module.exports = {
